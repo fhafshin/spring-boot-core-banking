8ae1194158cb3e030f6097719c3ac2ae7bb18e04
fhafshin <fhafshin@gmail.com>
2025-02-07 18:06:13 +0330

    start

---
+++ src/main/java/ir/setad/banking/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package ir.setad.banking.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+
+        return new OpenAPI().info(new Info()
+                .title("API CORE BANKING")
+                .description("develop banking core system")
+                .version("1.0"));
+
+    }
+}
